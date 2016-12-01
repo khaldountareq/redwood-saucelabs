a3f4d977c55169532cdace822d63cc63334422fb
John Doe <john@sample.com>
2016-12-01 19:03:09 +0200

    update bat file 3

--- EditHostFile_Windows.bat
+++ EditHostFile_Windows.bat
@@ -1,5 +1,5 @@
 @echo off
-echo  52.7.154.41 api.www.baaz.com  www.baaz.com  > %temp%\temphosts.txt
+echo  52.7.154.41 www.baaz.com  > %temp%\temphosts.txt
 echo  34.193.110.187 rtns.www.baaz.com >> %temp%\temphosts.txt
 echo  54.152.50.69  proxy1.baaz.com >> %temp%\temphosts.txt
 type C:\WINDOWS\system32\drivers\etc\hosts >> %temp%\temphosts.txt
